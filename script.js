b51498877bb2a480927b3e2d572143e63b4ca7d5
Nabil Ahmed <105531667+nabil-github0@users.noreply.github.com>
2023-06-15 21:26:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,103 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var dataArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")):[];
+var dataArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
+var inputToggleArray = localStorage.getItem("toggle") ? JSON.parse(localStorage.getItem("toggle")) : [];
+var currentYearElement = document.getElementById("currentYear");
+var currentYear = new Date().getFullYear();
+currentYearElement.textContent = currentYear;
 
 function inputLength() {
-	return input.value.length;
+  return input.value.length;
 }
 
-function addDataToLocalStorage () {
-	dataArray.push(input.value);
-	localStorage.setItem("items",JSON.stringify(dataArray));
+function addOrRemoveDataFromLocalStorage() {
+   localStorage.setItem("items", JSON.stringify(dataArray));
+   localStorage.setItem("toggle", JSON.stringify(inputToggleArray));
+}
+
+function addDataToLocalStorage() {
+  dataArray.push(input.value);
+  if (inputToggleArray.length < dataArray.length) {
+	inputToggleArray.push("");
+  }
+  addOrRemoveDataFromLocalStorage();
 }
 
 function removeDataFromLocalStorage(index) {
-	 dataArray.splice(index,1);
-     localStorage.setItem("items",JSON.stringify(dataArray));
+  dataArray.splice(index, 1);
+  inputToggleArray.splice(index, 1);
+  addOrRemoveDataFromLocalStorage();
+}
+
+function updateToggleDataToLocalStorage(element,index) {
+    if (element.classList.contains("done")) {
+		inputToggleArray[index] = "done";
+	  } else {
+		inputToggleArray[index] = "";
+	  }
+	  localStorage.setItem("toggle", JSON.stringify(inputToggleArray));
+  
+	if (inputToggleArray[index] === "done") {
+	  element.classList.add("done");
+	}
 }
 
 function viewData(data) {
-	var li = document.createElement("li");
-	var button = document.createElement("button");
-	var div = document.createElement("div");
-	var container = document.createElement("div");
-  
-	li.appendChild(document.createTextNode(data.charAt(0).toUpperCase() + data.slice(1)));
-	button.appendChild(document.createTextNode("Delete"));
-	div.append(li,button);
-	container.appendChild(div);
-	ul.appendChild(container);
-  
-	div.classList.add("oneLine");
-	button.classList.add("delete");
-	container.classList.add("item-container");
-	
-	button.addEventListener("click", function () {
-	  var parentDiv = button.parentElement.parentElement;
-	  var index = Array.from(ul.children).indexOf(parentDiv);
-	  parentDiv.remove();
-	  removeDataFromLocalStorage(index);
-	});
-  
-	li.addEventListener("click", function () {
-	  li.classList.toggle("done");
-	});
-  }
-  
+  var li = document.createElement("li");
+  var button = document.createElement("button");
+  var div = document.createElement("div");
+  var container = document.createElement("div");
+
+  li.appendChild(document.createTextNode(data.charAt(0).toUpperCase() + data.slice(1)));
+  button.appendChild(document.createTextNode("Delete"));
+  div.append(li, button);
+  container.appendChild(div);
+  ul.appendChild(container);
+
+  button.classList.add("delete");
+  div.classList.add("oneLine");
+  container.classList.add("item-container");
+
+  button.addEventListener("click", function () {
+    var parentDiv = button.parentElement.parentElement;
+    var index = Array.from(ul.children).indexOf(parentDiv);
+    parentDiv.remove();
+    removeDataFromLocalStorage(index);
+  });
+
+  li.addEventListener("click", function () {
+    var liParentDiv = li.parentElement.parentElement;
+    var index = Array.from(ul.children).indexOf(liParentDiv);
+    li.classList.toggle("done");
+    updateToggleDataToLocalStorage(li,index);
+
+})
+}
 
 function createListElement() {
-	   addDataToLocalStorage();
-	   viewData(input.value);
-	input.value = "";
+  addDataToLocalStorage();
+  viewData(input.value);
+  input.value = "";
 }
 
 function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+  if (inputLength() > 0) {
+    createListElement();
+  }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
 }
 
-dataArray.forEach(element => {
-	viewData(element)
-});
 
+dataArray.forEach((element, index) => {
+  viewData(element, index);
+});
 
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
-
